24fe28bafd75136a3b38ed473bcc411134b44dd4
Miguel Moreno <mmorenoher@gmail.com>
2020-04-24 07:43:39 -0700

    Tarea 1 - Meses MFMQ

---
+++ temp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] mes = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+
+            Console.Write("Introduzca el numero del mes ");
+            int numeroMes = 0;
+            bool esEntero = Int32.TryParse(Console.ReadLine(), out numeroMes);
+           
+            if (!esEntero || numeroMes < 1 || numeroMes > 12)
+            {
+                for (int i = 0; i < 13; i++)
+                {
+                    Console.WriteLine(mes[i]);
+                }
+            }
+
+            else 
+            {
+                Console.WriteLine("Seleccionaste el mes de: " + mes[numeroMes - 1]);
+            }
+        }
+    }
+}
